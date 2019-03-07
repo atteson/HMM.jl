438d1754e7c9c1ffcb64e3567f3127770e97b5ba
Kevin Atteson <kevin@atteson.com>
2019-03-07 09:08:47 -0500

    I think that's everything

--- script/profile.jl
+++ script/profile.jl
@@ -1,42 +1,30 @@
-using HMM
+using HMMs
 using Brobdingnag
 using Profile
 using ProfileView
 using GCTools
 
-hmm1 = HMM.randomhmm( HMM.fullyconnected(2), calc=Brob )
-y = HMM.rand( hmm1, 100000 );
+hmm1 = HMMs.randomhmm( HMMs.fullyconnected(2), calc=Brob )
+y = HMMs.rand( hmm1, 100000 );
 
-hmm2 = HMM.randomhmm( HMM.fullyconnected(2), calc=Brob, seed=2 )
-HMM.setobservations( hmm2, y );
-@time HMM.em( hmm2 )
-hmm2 = HMM.randomhmm( HMM.fullyconnected(2), calc=Brob, seed=2 )
-HMM.setobservations( hmm2, y );
+hmm2 = HMMs.randomhmm( HMMs.fullyconnected(2), calc=Brob, seed=2 )
+HMMs.setobservations( hmm2, y );
+@time HMMs.em( hmm2 )
+hmm2 = HMMs.randomhmm( HMMs.fullyconnected(2), calc=Brob, seed=2 )
+HMMs.setobservations( hmm2, y );
 GCTools.reset()
-@time HMM.em( hmm2 )
+@time HMMs.em( hmm2 )
 GCTools.print()
 
 Profile.clear()
-hmm2 = HMM.randomhmm( HMM.fullyconnected(2), calc=Brob, seed=2 )
-HMM.setobservations( hmm2, y );
-@profile HMM.em( hmm2 )
+hmm2 = HMMs.randomhmm( HMMs.fullyconnected(2), calc=Brob, seed=2 )
+HMMs.setobservations( hmm2, y );
+@profile HMMs.em( hmm2 )
 ProfileView.view()
 
-hmm2 = HMM.randomhmm( HMM.fullyconnected(2), calc=Brob, seed=2 )
-HMM.setobservations( hmm2, y );
-@time HMM.em( hmm2 )
+hmm2 = HMMs.randomhmm( HMMs.fullyconnected(2), calc=Brob, seed=2 )
+HMMs.setobservations( hmm2, y );
+@time HMMs.em( hmm2 )
 # 2.02s
 # 1.40s
 
-x = rand( 100000 )
-bx = convert( Vector{Brob}, x )
-GCTools.reset()
-GCTools.push!( :all )
-for i = 2:length(bx)
-    bx[i].positive = bx[i-1].positive
-    bx[i].log = bx[i-1].log
-end
-GCTools.pop!()
-GCTools.print()
-
-t = [T1(rand(1:1_000_000), rand(1:1_000_000)) for i in 1:100_000]
