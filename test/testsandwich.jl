e63b565e961c4fa5bf71257b89231ee7ee57c5ac
Kevin Atteson <kevin@atteson.com>
2019-03-14 19:26:28 -0400

    checking in to move

--- test/testsandwich.jl
+++ test/testsandwich.jl
@@ -21,6 +21,33 @@ Ihat2 = inv(-convert( Matrix{Float64}, sum( d2logb[2:3,2:3,1,1:end], dims=3 )[:,
 sigma = nhmm2.stateparameters[2,1]
 @assert( maximum(abs.(inv([1/sigma^2 0.0; 0.0 2/sigma^2]) - n*C[2:3,2:3])) < 0.01 )
 
+nhmm3 = HMMs.randomhmm( HMMs.fullyconnected(2), calc=Brob, seed=2 )
+n = 1_000_000
+y3 = rand( nhmm3, n );
+
+pathname = joinpath( homedir(), "experiments", "test", "nhmms2" )
+filename = joinpath( pathname, "hmm2.2" )
+if !isfile( filename )
+    nhmm4 = copy( nhmm3 )
+    HMMs.setobservations( nhmm4, y3 );
+    HMMs.em( nhmm4, debug=2, keepintermediates=true, acceleration=10 )
+
+    mkpath( pathname )
+    open( filename, "w" ) do file
+        HMMs.write( file, nhmm4 )
+    end
+else
+    nhmm4 = open( filename, "r" ) do file
+        read( file, HMMs.GaussianHMM{Brob, Float64} )
+    end
+end
+
+C = convert( Matrix{Float64}, HMMs.sandwich( nhmm4 ) )
+d2logl = HMMs.d2loglikelihood( nhmm4 );
+dlogl = HMMs.d2logprobabilities( nhmm4 );
+Ihat1 = inv(-convert( Matrix{Float64}, d2logl[:,:,end] )/n)
+@assert( maximum(abs.(Ihat1 - n*C[2:3,2:3])) < 0.01 )
+
 
 hmm1 = HMMs.randomhmm( HMMs.fullyconnected(1), dist=HMMs.GenTDist, calc=Brob, seed=1 )
 n = 1_000_000
@@ -64,4 +91,15 @@ Ihat2 = d2logl[2:4,2:4,end]
 
 @assert( convert( Float64, maximum(abs.(Ihat1 - Ihat2)./Ihat1) ) < 0.005 )
 
+hmm3 = HMMs.randomhmm( HMMs.fullyconnected(2), dist=HMMs.GenTDist, calc=Brob, seed=1 )
+n = 1_000_000
+y3 = rand( hmm3, n );
 
+hmm4 = copy( hmm3 )
+HMMs.setobservations( hmm4, y3 );
+HMMs.em( hmm4, debug=2 )
+
+C = convert( Matrix{Float64}, HMMs.sandwich( hmm4 ) )
+d2logl = HMMs.d2loglikelihood( hmm4 );
+Ihat1 = convert( Matrix{Float64}, d2logl[:,:,end]/n )
+n*C
