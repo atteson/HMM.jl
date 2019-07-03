6daf5f0b4c6755a9d98a7ab6e72c8c4b09f3b4b8
Kevin Atteson <kevin@atteson.com>
2019-07-03 13:51:19 -0400

    need to work on Distributions; probably as part of Models

--- src/HMMs.jl
+++ src/HMMs.jl
@@ -1,5 +1,41 @@
 module HMMs
 
 using Models
+using Distributions
+
+mutable struct HMM{N, Dist <: Distribution, Calc <: Real, T, U} <: Models.AbstractModel{T,U}
+    initialprobabilities::Vector{Calc}
+    currentprobabilities::Vector{Calc}
+    
+    transitionprobabilities::Matrix{Calc}
+    stateparameters::Vector{Calc}
+
+    # some linear equality constraints including transition probability unit row sums
+    constraintmatrix::Matrix{Calc}
+    constraintvector::Vector{Calc}
+end
+
+function Models.rand( ::Type{HMM{N,D,C,T,U}}; seed::Int = -1, ) where {N,D,C,T,U}
+    if seed >= 0
+        Random.seed!( seed )
+    end
+
+    initialprobabilities = Vector{C}( rand(N) )
+    initialprobabilities /= sum( initialprobabilities )
+    currentprobabilities = copy( initialprobabilities )
+
+    transitionprobabilities = Matrix{C}( rand(N,N) )
+    transitionprobabilities ./= sum( transitionprobabilities, dims=2 )
+
+    stateparameters = hcat( [randomparameters( D ) for i in 1:N]... )
+
+    p = size( stateparameters, 1 )
+    constraintmatrix = [0.0 + (N==1 || div(j-1,N)==i-1) for i in 1:N, j in 1:N^2]
+    constraintmatrix = [constraintmatrix zeros(N,N*p)]
+    constraintvector = ones(N)
+
+    return HMM( initialprobabilities, currentprobabilities, transitionprobabilities, stateparameters,
+                constraintmatrix, constraintvector )
+end
 
 end # module
--- test/testfit.jl
+++ test/testfit.jl
@@ -1,1 +1,4 @@
 using HMMs
+using Distributions
+
+rand( HMM{2,Gaussian,Float64,Int,Float64} )
