25a38c76d7024d4b725f40c10dbdc876e729da8a
Kevin Atteson <kevin@atteson.com>
2019-07-06 09:11:13 -0400

    saving changes before refactor

--- test/testsandwich.jl
+++ test/testsandwich.jl
@@ -46,3 +46,11 @@ n = 1_000_000
 N = 100
 data2 = pmap( fithmm, fill(n,N), fill(nhmm,N), 1:N )
 
+p = Models.getcompressedparameters( nhmm )
+vs = Vector{Float64}[]
+for i = 1:N
+    (p1, C1) = data1[i]
+    sC1 = real.(sqrt(C1))
+    @assert( maximum(abs.(C1 - sC1 * sC1')) < 1e-10 )
+    push!( vs, inv(sC1) * (p - p1) )
+end
